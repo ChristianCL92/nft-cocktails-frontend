d95afd1eaa17acd985dd051f3ec3722b61554cda
Christian Lopez <chrisdev92@outlook.com>
2024-12-18 17:40:38 +0100

    Add WalletContext and useWallet hook for managing wallet connections in a centralized manner

---
+++ Frontend-nft-cocktails/src/context/WalletContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useState, useEffect } from 'react';
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  walletListener,
+} from '../utilities/ContractConfig';
+
+ export const WalletContext = createContext();
+
+export const WalletProvider = ({ children }) => {
+  const [walletAddress, setWalletAddress] = useState('');
+  const [contract, setContract] = useState(null);
+  const [isConnecting, setIsConnecting] = useState(false);
+
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const { address, contract } = await getCurrentWalletConnected();
+        setWalletAddress(address);
+        setContract(contract);
+
+        if (address) {
+          walletListener(setWalletAddress);
+        }
+      } catch (error) {
+        console.log('error initializing wallet', error);
+      } 
+    };
+    initialize();
+  }, []);
+
+  const handleConnectWallet = async () => {
+    if (!window.ethereum) {
+      window.open('https://metamask.io/download/', '_blank');
+      return;
+    }
+
+    try {
+      setIsConnecting(true);
+      const { address, contract } = await connectWallet();
+      setWalletAddress(address);
+      setContract(contract);
+    } catch (error) {
+      console.error('Error connecting wallet', error);
+    } finally {
+      setIsConnecting(false);
+    }
+  };
+
+  const value = {
+    walletAddress,
+    contract,
+    isConnecting,
+    handleConnectWallet,
+  };
+
+  return (
+    <WalletContext.Provider value={value}>
+        {children}
+    </WalletContext.Provider>
+  );
+};
+
