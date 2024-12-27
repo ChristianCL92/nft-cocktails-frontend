9db20969156d2c6d49f96a2ba85258517974d74d
Christian Lopez <chrisdev92@outlook.com>
2024-12-27 16:16:42 +0100

    Adding a p tag stating the projects educational purpose

--- Frontend-nft-cocktails/src/components/CocktailNFTS.jsx
+++ Frontend-nft-cocktails/src/components/CocktailNFTS.jsx
@@ -8,12 +8,16 @@ import UserCollection from './UserCollection';
 const CocktailNFTS = () => {
 
   return (
-    <div className='container'>
-    <Header/>
-    <CocktailCollection/>
-    <UserCollection />
+    <div className="container">
+      <Header />
+      <CocktailCollection />
+      <UserCollection />
+      <p className="disclaimer">
+        This is an educational project focused on blockchain technology and
+        NFTs. The content is for demonstration purposes only and not intended to
+        promote alcohol consumption.
+      </p>
     </div>
-  
   );
 };
 
